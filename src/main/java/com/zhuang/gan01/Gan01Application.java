5435150e92983d44e37e2b10683f3b79bb4cba5e
zhuangxin <18795975110@163.com>
2022-06-21 18:32:13 +0800

    Initial commit

---
+++ src/main/java/com/zhuang/gan01/Gan01Application.java
@@ -0,0 +1,13 @@
+package com.zhuang.gan01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gan01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gan01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhuang/gan01/Gan01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuang.gan01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gan01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
